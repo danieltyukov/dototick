36e6fa3a7752573cf42b1b4a2daab25aa3e26719
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-01-11 00:55:39 +0200

    added tiles

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,27 +10,110 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List todos = List();
+  String inputtask = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: MyConstants.darkgrey,
-        bottomNavigationBar: bottomnav(context),
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            "Today Schedule",
-            textAlign: TextAlign.center,
-          ),
+      backgroundColor: MyConstants.darkgrey,
+      bottomNavigationBar: bottomnav(context),
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          "Today Schedule",
+          textAlign: TextAlign.center,
         ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: MyConstants.lightgrey,
-          child: Icon(
-            Icons.add,
-            color: Colors.white,
-            size: 30,
-          ),
-          onPressed: () {},
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 0,
+        backgroundColor: MyConstants.lightgrey,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 30,
         ),
-        body: Column());
+        onPressed: _showDialog,
+      ),
+      body: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+              direction: DismissDirection.startToEnd,
+              onDismissed: (direction) => todos.removeAt(index),
+              key: UniqueKey(),
+              child: Card(
+                elevation: 4,
+                margin: EdgeInsets.all(4),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: ListTile(
+                  title: Text(todos[index]),
+                ),
+              ),
+            );
+          }),
+    );
+  }
+
+  _showDialog() async {
+    return await showDialog<String>(
+      context: context,
+      child: AlertDialog(
+        elevation: 0,
+        backgroundColor: MyConstants.blue,
+        contentPadding: const EdgeInsets.all(16.0),
+        content: Row(
+          children: <Widget>[
+            Expanded(
+              child: TextField(
+                onChanged: (String value) {
+                  inputtask = value;
+                },
+                cursorColor: Colors.white,
+                style: TextStyle(color: Colors.white70),
+                autofocus: true,
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white)),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  labelText: 'Name Of Task',
+                ),
+              ),
+            )
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text(
+              'CANCEL',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          FlatButton(
+            onPressed: () {
+              setState(() {
+                todos.add(inputtask);
+              });
+              Navigator.pop(context);
+            },
+            child: const Text(
+              'SUBMIT',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
