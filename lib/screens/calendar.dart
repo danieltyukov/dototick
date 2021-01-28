1b1529f7a15f8702851ddf991e44f65bb57a9624
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-01-28 17:58:36 +0200

    added more functions for the adding functionality and display

--- lib/screens/calendar.dart
+++ lib/screens/calendar.dart
@@ -21,55 +21,46 @@ class _CalendarState extends State<Calendar> {
   //
   DateTime _selectedDay = DateTime.now();
 
-  CalendarController _calenderController;
-
+  CalendarController _calendarController;
   Map<DateTime, List<dynamic>> _events = {};
+  List<CalendarItem> _data = [];
 
   List<dynamic> _selectedEvents = [];
-
   List<Widget> get _eventWidgets =>
       _selectedEvents.map((e) => events(e)).toList();
 
-  //
-  @override
   void initState() {
     super.initState();
-    // DB.init().then((value) => _fetchEvents());
-    _calenderController = CalendarController();
+    DB.init().then((value) => _fetchEvents());
+    _calendarController = CalendarController();
   }
 
-  @override
   void dispose() {
-    _calenderController.dispose();
+    _calendarController.dispose();
     super.dispose();
   }
 
-  //
   Widget events(var d) {
     return Container(
       padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
       child: Container(
-        width: MediaQuery.of(context).size.width * 0.9,
-        padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
-        decoration: BoxDecoration(
-            border: Border(
-          top: BorderSide(color: Theme.of(context).dividerColor),
-        )),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
+          width: MediaQuery.of(context).size.width * 0.9,
+          padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+          decoration: BoxDecoration(
+              border: Border(
+            top: BorderSide(color: Theme.of(context).dividerColor),
+          )),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
             Text(d, style: Theme.of(context).primaryTextTheme.bodyText1),
             IconButton(
-              icon: FaIcon(
-                FontAwesomeIcons.trashAlt,
-                color: Colors.redAccent,
-                size: 15,
-              ),
-              // onPressed: () => _deleteEvent(d),
-            )
-          ],
-        ),
-      ),
+                icon: FaIcon(
+                  FontAwesomeIcons.trashAlt,
+                  color: Colors.redAccent,
+                  size: 15,
+                ),
+                onPressed: () => _deleteEvent(d))
+          ])),
     );
   }
 
@@ -97,36 +88,91 @@ class _CalendarState extends State<Calendar> {
         _name = value;
       },
     );
-
-    //
-
     var btn = FlatButton(
-      child: Text(
-        'Save',
-        style: GoogleFonts.montserrat(
-            color: Color.fromRGBO(59, 57, 60, 1),
-            fontSize: 16,
-            fontWeight: FontWeight.bold),
-      ),
+      child: Text('Save',
+          style: GoogleFonts.montserrat(
+              color: Color.fromRGBO(59, 57, 60, 1),
+              fontSize: 16,
+              fontWeight: FontWeight.bold)),
       onPressed: () => _addEvent(_name),
     );
-
-    //
-
     var cancelButton = FlatButton(
-      child: Text(
-        'Cancel',
-        style: GoogleFonts.montserrat(
-            color: Color.fromRGBO(59, 57, 60, 1),
-            fontSize: 16,
-            fontWeight: FontWeight.bold),
+        child: Text('Cancel',
+            style: GoogleFonts.montserrat(
+                color: Color.fromRGBO(59, 57, 60, 1),
+                fontSize: 16,
+                fontWeight: FontWeight.bold)),
+        onPressed: () => Navigator.of(context).pop(false));
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(6),
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(6),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(6),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10.0,
+                    offset: const Offset(0.0, 10.0),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min, // To make the card compact
+                children: <Widget>[
+                  SizedBox(height: 16.0),
+                  Text("Add Event",
+                      style: GoogleFonts.montserrat(
+                          color: Color.fromRGBO(59, 57, 60, 1),
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold)),
+                  Container(padding: EdgeInsets.all(20), child: content),
+                  Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[btn, cancelButton]),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-      onPressed: () => Navigator.of(context).pop(false),
     );
+  }
 
-    //
+  void _fetchEvents() async {
+    _events = {};
+    List<Map<String, dynamic>> _results = await DB.query(CalendarItem.table);
+    _data = _results.map((item) => CalendarItem.fromMap(item)).toList();
+    _data.forEach((element) {
+      DateTime formattedDate = DateTime.parse(DateFormat('yyyy-MM-dd')
+          .format(DateTime.parse(element.date.toString())));
+      if (_events.containsKey(formattedDate)) {
+        _events[formattedDate].add(element.name.toString());
+      } else {
+        _events[formattedDate] = [element.name.toString()];
+      }
+    });
+    setState(() {});
+  }
 
-    
+  void _addEvent(String event) async {
+    CalendarItem item =
+        CalendarItem(date: _selectedDay.toString(), name: event);
+    await DB.insert(CalendarItem.table, item);
+    _selectedEvents.add(event);
+    _fetchEvents();
+
+    Navigator.pop(context);
   }
 
   //
@@ -140,20 +186,22 @@ class _CalendarState extends State<Calendar> {
             padding: EdgeInsets.all(15),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [],
+              children: [
+                Text(
+                  'Calender',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
             ),
           ),
-
-          // calendar(),
-          // eventTitle(),
-          // Column(children: _eventWidgets),
-          SizedBox(
-            height: 60,
-          )
+          calendar(),
+          eventTitle(),
+          Column(children: _eventWidgets),
+          SizedBox(height: 60),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        // onPressed: () => _create(context),
+        onPressed: () => _create(context),
         child: Icon(
           Icons.add,
           color: Colors.white,
