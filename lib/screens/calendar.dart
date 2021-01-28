2e3d8ac7680882604a24a28675b2a5df25e5e705
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-01-28 16:31:28 +0200

    events and ondayselected function made

--- lib/screens/calendar.dart
+++ lib/screens/calendar.dart
@@ -20,21 +20,87 @@ class Calendar extends StatefulWidget {
 class _CalendarState extends State<Calendar> {
   //
   DateTime _selectedDay = DateTime.now();
-  CalendarController _callenderController;
+
+  CalendarController _calenderController;
+
   Map<DateTime, List<dynamic>> _events = {};
+
   List<dynamic> _selectedEvents = [];
+
   List<Widget> get _eventWidgets =>
       _selectedEvents.map((e) => events(e)).toList();
 
-    
   //
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    DB.ini
+    // DB.init().then((value) => _fetchEvents());
+    _calenderController = CalendarController();
   }
-    
+
+  @override
+  void dispose() {
+    _calenderController.dispose();
+    super.dispose();
+  }
+
+  //
+  Widget events(var d) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.9,
+        padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+        decoration: BoxDecoration(
+            border: Border(
+          top: BorderSide(color: Theme.of(context).dividerColor),
+        )),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(d, style: Theme.of(context).primaryTextTheme.bodyText1),
+            IconButton(
+              icon: FaIcon(
+                FontAwesomeIcons.trashAlt,
+                color: Colors.redAccent,
+                size: 15,
+              ),
+              // onPressed: () => _deleteEvent(d),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onDaySelected(DateTime day, List events) {
+    setState(() {
+      _selectedDay = day;
+      _selectedEvents = events;
+    });
+  }
+
+  void _create(BuildContext context) {
+    String _name = "";
+    var content = TextField(
+      style: GoogleFonts.montserrat(
+          color: Color.fromRGBO(105, 105, 108, 1), fontSize: 16),
+      autofocus: true,
+      decoration: InputDecoration(
+        labelStyle: GoogleFonts.montserrat(
+            color: Color.fromRGBO(59, 57, 60, 1),
+            fontSize: 18,
+            fontWeight: FontWeight.normal),
+        labelText: 'Workout Name',
+      ),
+      onChanged: (value) {
+        _name = value;
+      },
+    );
+
+    //
+  }
+
   //
   @override
   Widget build(BuildContext context) {
@@ -42,6 +108,14 @@ class _CalendarState extends State<Calendar> {
       backgroundColor: MyConstants.darkgrey,
       body: ListView(
         children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [],
+            ),
+          ),
+
           // calendar(),
           // eventTitle(),
           // Column(children: _eventWidgets),
