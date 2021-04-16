807e88dcf7f407fc43296d6ef449b90f084b71cb
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-04-16 14:37:24 +0300

    removed the previous databases and added final new

--- lib/db/weektasks_model.dart
+++ lib/db/weektasks_model.dart
@@ -7,5 +7,25 @@ class Weektasks {
 
   Weektasks({this.id, this.day, this.name});
 
-  factory Weektasks.fromMap(Map<String, dynamic> json) => new Weektasks();
+  factory Weektasks.fromMap(Map<String, dynamic> json) => new Weektasks(
+        id: json["id"],
+        day: json["day"],
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "day": day,
+        "name": name,
+      };
+}
+
+Weektasks weektasksFromJson(String str) {
+  final jsonData = json.decode(str);
+  return Weektasks.fromMap(jsonData);
+}
+
+String weektasksToJson(Weektasks data) {
+  final dyn = data.toMap();
+  return json.encode(dyn);
 }
